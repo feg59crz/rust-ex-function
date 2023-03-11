6d8356869925b457b0bf0cfad3aed7882412958f
Fernando Cruz <fernandogomescruz@gmail.com>
2023-03-11 17:10:41 -0300

    function with return

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
-    println!("Hello, world!");
-    
     another_function(5);
     print_labeled_measurement(5, 'h');
+
+    let y = five();
+
+    println!("The value of y is: {y}");
 }
 
 fn another_function(x: i32) {
@@ -11,4 +13,8 @@ fn another_function(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char){
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
 }
